05c04584ac348723f0310eb1b71bcbbf819a85c0
Barbara Quecedo <barbaraquecedo@gmail.com>
2021-11-25 14:36:04 +0100

    50-50 done

--- index.js
+++ index.js
@@ -1,17 +1,66 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = 0;
 
-  add(item) {}
 
-  get(pos) {}
 
-  max() {}
+  }
 
-  min() {}
+  add(item) {
+    this.items.push(item);
+    this.length = this.items.length;
+    this.items.sort((a, b) => {
+      return a - b
+    })
+  }
 
-  sum() {}
 
-  avg() {}
+  get(pos) {
+
+    if (this.items(pos) === null) {
+      throw new Error('OutOfBounds');
+
+    } 
+    return this.items.indexOf(pos)
+    
+  }
+  
+
+
+  max() {
+
+    if (this.items[0]) {
+      throw new Error('EmptySortedList');
+    } 
+    return Math.max(this.items[item]);
+  }
+
+  min() {
+
+    if (this.items[0]) {
+      throw new Error('EmptySortedList');
+    } 
+    return Math.min(this.items[item]);
+  }
+
+
+
+  sum() {
+    
+    if (this.items[0]) {
+      this.items === 0;
+    } 
+    
+    this.items.reduce((a, b) => {
+      return a + b;
+    });
+  }
+
+
+  avg() {
+
+  }
 }
 
 module.exports = SortedList;
